0de2d8b369140a0b8500c695ce37fba871e792fb
Ali Mohamed <74410154+alin00r@users.noreply.github.com>
2024-09-04 01:06:04 +0300

    Time: 57 ms (68.87%) | Memory: 51.4 MB (82.86%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let prev = null;
+    let temp = head;   
+    let next;
+    while(temp){
+        next = temp.next;
+        temp.next = prev;
+        prev= temp
+        temp = next
+        
+    }
+    return prev
+};
