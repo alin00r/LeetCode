a3beca5042129506b49b401201ed16b39c3806f2
Ali Mohamed <74410154+alin00r@users.noreply.github.com>
2024-09-11 22:40:32 +0300

    Time: 143 ms (49.49%) | Memory: 76.2 MB (44.91%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.js
+++ 234-palindrome-linked-list/palindrome-linked-list.js
@@ -1,10 +1,8 @@
 var isPalindrome = function(head) {
-    let  str1 = str2 = "";
-    let temp = head;
-    while(temp){
-        str1=`${temp.val}${str1}`
-        str2=`${str2}${temp.val}`
-        temp= temp.next;
+    const arr = [];
+    while(head && head.val !== null) {
+        arr.push(head.val);
+        head = head.next;
     }
-    return str1===str2;
-}
+    return arr.every((e, i) => e === arr[arr.length - i - 1]);
+};
