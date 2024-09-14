04de9affdb825214b01ba73e711d49f11db4dd48
Ali Mohamed <74410154+alin00r@users.noreply.github.com>
2024-09-14 17:35:46 +0300

    Time: 114 ms (83.35%) | Memory: 69.3 MB (88.46%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.js
+++ 234-palindrome-linked-list/palindrome-linked-list.js
@@ -1,28 +1,27 @@
-var isPalindrome = function(head) {
-    if (head === null || head.next === null) return true;
+var isPalindrome = (head) => {
+    if(head === null || head.next === null) return true
+    // get the middle of the linkedlist
     let slow = head;
     let fast = head;
-
-    while (fast !== null && fast.next !== null) {
+    while(fast && fast.next){
         slow = slow.next;
         fast = fast.next.next;
     }
-
+    // reverse the secondHalf of the linkedlist .
     let prev = null;
-    while (slow !== null) {
-        let temp = slow.next;
+    while(slow !== null){
+        let next = slow.next;
         slow.next = prev;
         prev = slow;
-        slow = temp;
+        slow = next ;
     }
-
+    // compare the secondHalf with the firstHalf element by element.
     let firstHalf = head;
-    let secondHalf = prev; 
-    while (secondHalf !== null) {
-        if (firstHalf.val !== secondHalf.val) return false;
+    let secondHalf = prev;
+    while(secondHalf !== null){
+        if(firstHalf.val !== secondHalf.val) return false;
         firstHalf = firstHalf.next;
         secondHalf = secondHalf.next;
     }
-
     return true;
 };
