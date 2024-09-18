76516945c1e8fb037fdf681c5de0c3ff67b438f7
Ali Mohamed <74410154+alin00r@users.noreply.github.com>
2024-09-18 05:01:53 +0300

    Time: 83 ms (43.69%) | Memory: 62.3 MB (52.45%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -3,12 +3,6 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    nums.sort((a,b)=>a-b)
-    for (let i=0;i<nums.length-1;i++) {
-        if(nums[i]==nums[i+1]){
-        return true
-    }}
-  return false
-        
-    
+    const visited = new Set(nums);
+    return visited.size !== nums.length
 };
